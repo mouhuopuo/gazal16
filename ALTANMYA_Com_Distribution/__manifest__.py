3155e345d355b580687811a8e5088eb9855ec130
ANASNADERALGENAIMI <142088414+ANASNADERALGENAIMI@users.noreply.github.com>
2023-08-14 16:51:18 +0300

    Add files via upload

--- ALTANMYA_Com_Distribution/__manifest__.py
+++ ALTANMYA_Com_Distribution/__manifest__.py
@@ -1,15 +1,22 @@
 {
-    'name': 'Company Distrubution',
+    'name': 'Employee Info',
     'author': 'Odoo mates',
     'sequence': -1000,
     'website': 'www.odoomates.tech',
     'summary': 'Odoo 16 Developer',
-    'depends': ['account_accountant'],
+    'depends': [
+        'account_accountant',
+        'hr_contract_salary',
+        'hr_payroll',
+        'report_xlsx', 'web', ],
     'data': [
         'security/ir.model.access.csv',
-        'data/sequence.xml',
-        'data/cron_action.xml',
-        'views/Accounting_menu.xml',
+        'wizards/create_excel_wizard.xml',
+        'views/employee_info.xml',
+        'views/emp_inherit.xml',
+        'views/payslip_inherit.xml',
+        'report/report.xml',
 
-    ]
+    ],
+    'license': 'LGPL-3',
 }
